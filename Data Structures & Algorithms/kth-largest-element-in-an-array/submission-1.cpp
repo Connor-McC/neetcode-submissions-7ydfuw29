cd1dc1a50b58ada17218d9d3a46744ed9e18892d
Connor-McC <75641724+Connor-McC@users.noreply.github.com>
2026-07-06 11:04:43 -0700

    Add: kth-largest-element-in-an-array - submission-1

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> maxHeap;
+
+        for (int num : nums){
+            maxHeap.push(num);
+        }
+
+        for (int i = 0; i < k-1; i++){
+            maxHeap.pop();
+        }
+
+        return maxHeap.top();
+    }
+};
