114e1585362a22b2f0d65bbc85a198a174692eab
Connor-McC <75641724+Connor-McC@users.noreply.github.com>
2026-07-16 15:28:34 -0700

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        vector<int> subset;
+        backtrack(nums, 0, subset, result);
+        return result;
+    }
+
+private:
+    void backtrack(vector<int>& nums, int start, vector<int>& subset, vector<vector<int>>& result) {
+        result.push_back(subset);
+
+        for (int i = start; i < nums.size(); i++) {
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue; // skip duplicate at this recursion level
+            }
+            subset.push_back(nums[i]);
+            backtrack(nums, i + 1, subset, result);
+            subset.pop_back();
+        }
+    }
+};
